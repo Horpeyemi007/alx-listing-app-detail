ed166560cf4affce957e6a90c3f7d19b0752e290
Opeyemi <Opeyemi5055@yahoo.com>
2025-08-02 01:22:52 +0100

    first commit

---
+++ components/property/ReviewSection.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ReviewSection: React.FC<{ reviews: any[] }> = ({ reviews }) => {
+  return (
+    <div className="mt-8 p-8">
+      <h3 className="text-2xl font-semibold">Review</h3>
+      {reviews.map((review, index) => (
+        <div key={index} className="pb-4 mb-4">
+          <div className="flex items-center">
+            <img
+              src={review.avatar}
+              alt={review.name}
+              className="w-12 h-12 rounded-full mr-4"
+            />
+            <div>
+              <p className="font-bold">{review.name}</p>
+              <p className="text-yellow-500">{review.rating} starts</p>
+            </div>
+          </div>
+          <p>{review.comment}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReviewSection;
